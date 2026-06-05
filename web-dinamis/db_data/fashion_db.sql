230929706a6b191b6cf07d3adb348a7886e499d7
ReviInjani <reviinjani24@gmail.com>
2026-06-05 12:56:38 +0700

    perbaiki foto cv

--- web-dinamis/db_data/fashion_db.sql
+++ web-dinamis/db_data/fashion_db.sql
@@ -1,7 +1,7 @@
--- Database: fashion_db
+-- Database: dbcompro_2388010027
 
-CREATE DATABASE IF NOT EXISTS fashion_db;
-USE fashion_db;
+CREATE DATABASE IF NOT EXISTS dbcompro_2388010027;
+USE dbcompro_2388010027;
 
 -- Table structure for table `users`
 CREATE TABLE IF NOT EXISTS `users` (
