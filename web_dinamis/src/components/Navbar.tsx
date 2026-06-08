9e9819f627b4caa17d392d290ae6e9dde70e5771
ReviInjani <reviinjani24@gmail.com>
2026-06-08 12:57:27 +0700

    hapus nim

--- web_dinamis/src/components/Navbar.tsx
+++ web_dinamis/src/components/Navbar.tsx
@@ -24,7 +24,7 @@ export default function Navbar() {
   return (
     <nav className={`navbar ${scrolled ? "navbar--scrolled" : ""}`}>
       <Link href="/" className="logo">
-        Injani's Fashion - 2388010009
+        Injani's Fashion
       </Link>
 
       {/* Desktop */}
